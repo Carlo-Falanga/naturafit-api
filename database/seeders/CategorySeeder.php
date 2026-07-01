03a52ad77dfb534615e83122950713696a0ab88d
Carlo-Falanga <carlofalanga7@gmail.com>
2026-07-01 15:39:41 +0200

    Add CategorySeeder to populate initial category data

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Colazione',
+            'Pranzo',
+            'Cena',
+            'Snack',
+            'Dolci',
+            'Primi piatti',
+            'Secondi piatti',
+            'Contorni',
+        ];
+
+        foreach($categories as $category){
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->save();
+        }
+    }
+}
