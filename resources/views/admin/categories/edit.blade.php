845216d981e04ee30691b645714bc2876011ec81
Carlo-Falanga <carlofalanga7@gmail.com>
2026-07-03 10:56:34 +0200

    Add edit category view and update edit method in CategoryController

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nuova Categoria</title>
+</head>
+<body>
+    <form action="{{ route('admin.categories.update', $category->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <label for="name">Nome</label>
+        <input type="text" name="name" id="name" value="{{ $category->name }}">
+
+        <button type="submit">Salva</button>
+    </form>
+</body>
+</html>
