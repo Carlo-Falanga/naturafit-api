6dcfce18041ef81bb700195e492961ca1ec9043e
Carlo-Falanga <carlofalanga7@gmail.com>
2026-07-07 10:13:33 +0200

    Add RecipeController, API routes, and Sanctum configuration for authentication

---
+++ app/Http/Controllers/Api/RecipeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    public function index(){
+        return 'Index API';
+    }
+}
